34e2a2f26044273b9d40d728fe3585a61ede47ea
Sergio Melendez <sergiomelendez026@gmail.com>
2023-11-10 17:38:02 -0500

    subiendo al repo

---
+++ Documents/AdaSchools/Express/server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express();
+const port = 4000;
+
+const listaTareas = [
+    {"id": "1", "isComplete": "false", "description": "walk the dog"},
+    {"id": "2", "isComplete": "false", "description": "clean my house"}
+]
+
+app.get("/", (req, res) => {
+    res.json(listaTareas);
+});
+
+
+
+app.listen(port, () => {
+    console.log("server listening on " + port);
+}); 
+
+
+
+
+
+
+
+
+
+
+
