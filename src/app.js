357fc8675d6cba4acb9a7f40269f4f8f96116d0d
Sergio Melendez <sergiomelendez026@gmail.com>
2023-11-15 17:55:55 -0500

    subiendo al repo laboratorio

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 // TODO: Completa tu código aquí ⬇️
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo');
+});
+
+app.get('/mensaje', (req, res) => {
+  res.send('este es mi servidor usando express');
+});
 
 //Se recomienda no editar ni eliminar la instancia del servidor.
 // Instancia del servidor
